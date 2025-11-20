c20433ee3afe121359b4591d86a756cb9fb37646
Joody20 <loveed123@naver.com>
2025-11-20 18:25:45 +0900

    refactor: API 부분 리팩토링

--- js/utils/user.js
+++ js/utils/user.js
@@ -1,3 +1,4 @@
+import { apiRequest } from "./api.js";
 const profileImg = document.getElementById("profile_img");
 const dropdownMenu = document.getElementById("dropdown_menu");
 
@@ -14,40 +15,30 @@ window.addEventListener("click", (e) => {
 export async function loadUserProfile() {
   try {
     const userId = localStorage.getItem("userId");
-    const accessToken = localStorage.getItem("access_token");
-
-    if (!userId || !accessToken) {
+    if (!userId) {
       console.warn("로그인 정보가 없습니다.");
       window.location.href = "/login.html";
       return;
     }
 
-    const res = await fetch(`http://localhost:8080/users/${userId}/profile`, {
+    // fetch 대신 apiRequest 사용!
+    const { ok, data } = await apiRequest(`/users/${userId}/profile`, {
       method: "GET",
-      credentials: "include", // ⭐ refresh token 자동 포함
-      headers: {
-        Authorization: `Bearer ${accessToken}`, // ⭐ access token 직접 포함
-      },
     });
 
-    // Access Token 만료 → 백엔드에서 401 제공
-    if (res.status === 401) {
+    if (!ok) {
       alert("로그인이 만료되었습니다. 다시 로그인해주세요.");
       window.location.href = "/login.html";
       return;
     }
 
-    const json = await res.json();
+    const imgUrl = data.data.profileImage;
 
-    if (json.message === "read_success") {
-      const imgUrl = json.data.profileImage;
-
-      profileImg.src = imgUrl
-        ? imgUrl.startsWith("http")
-          ? imgUrl
-          : `http://localhost:8080${imgUrl}`
-        : "./img/original_profile.png";
-    }
+    profileImg.src = imgUrl
+      ? imgUrl.startsWith("http")
+        ? imgUrl
+        : `http://localhost:8080${imgUrl}`
+      : "./img/original_profile.png";
   } catch (err) {
     console.error("프로필 불러오기 실패:", err);
   }
