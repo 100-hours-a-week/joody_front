b1282de807132779e8fe8d9f875d93594dead56c
Joody20 <loveed123@naver.com>
2025-11-27 23:24:40 +0900

    refactor:회원가입 헤더 리펙토링

--- components/signup/SignupHeader.js
+++ components/signup/SignupHeader.js
@@ -1,12 +1,12 @@
 import { h } from "../../vdom/Vdom.js";
 
-export default function SignupHeader() {
+export default function SignupHeader({ backPath }) {
   return h(
     "header",
     { id: "main_header" },
     h(
       "a",
-      { id: "back_link", onclick: () => (location.hash = "#/login") },
+      { id: "back_link", onclick: () => (location.hash = backPath) },
       h("img", {
         id: "back_button",
         src: "./assets/img/back_1.png",
