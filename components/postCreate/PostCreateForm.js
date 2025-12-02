f4d16d910f29ebe9efd70b7ac78b1fe057f2840d
Joody20 <loveed123@naver.com>
2025-12-02 22:56:50 +0900

    feat: 게시글 입력 폼 컴포넌트 조합

---
+++ components/postCreate/PostCreateForm.js
@@ -0,0 +1,34 @@
+import { h } from "../../vdom/Vdom.js";
+import PostCreateTitleInput from "./PostCreateTitleInput.js";
+import PostCreateContentInput from "./PostCreateContentInput.js";
+import PostCreateImageInput from "./PostCreateImageInput.js";
+import PostCreateButton from "./PostCreateButton.js";
+import HelperText from "../common/HelperText.js";
+
+export default function PostCreateForm({ state, handlers }) {
+  return h(
+    "form",
+    { id: "edit_form" },
+
+    PostCreateTitleInput({
+      value: state.title,
+      oninput: handlers.handleTitleInput,
+    }),
+
+    PostCreateContentInput({
+      value: state.content,
+      oninput: handlers.handleContentInput,
+    }),
+
+    HelperText({ message: state.helper }), // ⭐ 공통 헬퍼텍스트 사용 !!
+
+    PostCreateImageInput({
+      onSelect: handlers.handleImageSelect,
+    }),
+
+    PostCreateButton({
+      active: state.submitActive,
+      onSubmit: handlers.handleSubmit,
+    })
+  );
+}
