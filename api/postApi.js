91050189730359c48a17146b272e1b4a8e4c01cc
Joody20 <loveed123@naver.com>
2025-12-03 00:23:10 +0900

    feat: loadPostData 함수 추가

--- api/postApi.js
+++ api/postApi.js
@@ -1,4 +1,5 @@
 import { apiRequest } from "./authApi.js";
+import { setState } from "../vdom/Store.js";
 
 export async function fetchPostDetail(postId) {
   return apiRequest(`/posts/${postId}`);
@@ -6,4 +7,20 @@ export async function fetchPostDetail(postId) {
 
 export async function deletePostApi(postId) {
   return apiRequest(`/posts/${postId}`, { method: "DELETE" });
+}
+
+export async function loadPostData(postId) {
+  const { ok, data } = await apiRequest(`/posts/${postId}`);
+
+  if (!ok || !data) {
+    alert("게시글 로딩 중 오류 발생!");
+    location.href = "#/postlist";
+    return;
+  }
+
+  setState({
+    title: data.title,
+    content: data.content,
+    currentImageName: data.postImage ? data.postImage.split("/").pop() : "없음",
+  });
 }
