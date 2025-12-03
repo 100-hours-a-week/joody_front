ceddad2f10f2d56d508c2e1ac9dd5e37188eb751
Joody20 <loveed123@naver.com>
2025-12-03 17:38:58 +0900

    feat: 댓글 입력 분리

---
+++ components/postDetail/commentForm.js
@@ -0,0 +1,22 @@
+import { h } from "../../vdom/Vdom.js";
+
+export function CommentForm() {
+  return h(
+    "div",
+    { id: "comment_write_box" },
+    h("textarea", {
+      id: "comment_input",
+      placeholder: "댓글을 남겨주세요!",
+      "aria-label": "댓글 입력",
+    }),
+    h(
+      "button",
+      {
+        id: "submit_comment_button",
+        disabled: true,
+        style: { backgroundColor: "#d9d9d9" },
+      },
+      "댓글 등록"
+    )
+  );
+}
